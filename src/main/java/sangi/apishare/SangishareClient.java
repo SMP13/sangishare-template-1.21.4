89dd98c102c1c35ee313d98a3419a5802aaa2cb9
georg <georgtomaev04@gmail.com>
2024-12-20 10:51:41 +0300

    Initial commit

---
+++ src/main/java/sangi/apishare/SangishareClient.java
@@ -0,0 +1,10 @@
+package sangi.apishare;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SangishareClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/sangi/apishare/SangishareDataGenerator.java
@@ -0,0 +1,11 @@
+package sangi.apishare;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SangishareDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
